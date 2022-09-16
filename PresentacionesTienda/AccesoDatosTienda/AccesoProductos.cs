c847c7af955fc6537c5521d52ebb043f11f1fccd
Yarely <yarelyperez628@gmail.com>
2022-09-16 17:09:03 -0500

    commit Formulario y Procedures de BD

--- PresentacionesTienda/AccesoDatosTienda/AccesoProductos.cs
+++ PresentacionesTienda/AccesoDatosTienda/AccesoProductos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,23 @@ namespace AccesoDatosTienda
 {
     public class AccesoProductos
     {
+        Base b = new Base("localhost", "root", "", "tienda");
+
+        public void Borrar(dynamic Entidad)
+        {
+            b.Comando(string.Format("call deleteproducto({0})", Entidad.IdProducto));
+        }
+
+        public void Guardar(dynamic Entidad)
+        {
+            b.Comando(string.Format("call insertarproducto('{0}',{1},'{2}',{3})",
+                Entidad.Nombre, Entidad.Descripcion, Entidad.Precio, Entidad.IdsProducto));
+        }
+
+        public DataSet Mostrar(string filtro)
+        {
+            return b.Obtener
+                (string.Format("call showproducto('%{0}%')", filtro), "producto");
+        }
     }
 }
--- PresentacionesTienda/EntidadesTienda/Producto.cs
+++ PresentacionesTienda/EntidadesTienda/Producto.cs
@@ -8,5 +8,17 @@ namespace EntidadesTienda
 {
     public class Producto
     {
+        public Producto(int idProducto, string nombre, string descripcion, string precio)
+        {
+            IdProducto = idProducto;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+        }
+
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Precio { get; set; }
     }
 }
